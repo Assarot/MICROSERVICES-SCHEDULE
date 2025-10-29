23216a12f362440806679f0fdbfda189d0308ddf
Assarot <saemnapan@gmail.com>
2025-10-28 20:11:46 -0500

    fix

--- microservice_inventory/src/main/java/pe/edu/upeu/microservice_inventory/domain/port/out/CloudinaryPort.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upeu.microservice_inventory.domain.port.out;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface CloudinaryPort {
-    String uploadImage(MultipartFile file);
-    void deleteImage(String publicId);
-}
