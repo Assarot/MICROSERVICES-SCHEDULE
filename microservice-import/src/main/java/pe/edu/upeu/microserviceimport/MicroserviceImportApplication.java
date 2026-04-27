d8faf626ce35b12dfbeb61ab39a645257d178ea2
Assarot <saemnapan@gmail.com>
2026-04-27 07:44:36 -0500

    estructura base

---
+++ microservice-import/src/main/java/pe/edu/upeu/microserviceimport/MicroserviceImportApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.microserviceimport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceImportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceImportApplication.class, args);
+	}
+
+}
